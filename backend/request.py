634e04cf16aa1eb3b7251d2590fa8f62b901317d
ethankuning <franciscusmarselino@gmail.com>
2024-05-25 22:20:27 +0700

    fix: api endpoint

--- backend/request.py
+++ backend/request.py
@@ -1,4 +1,15 @@
 import requests
 
+query_text = "Kemarin saya putus dengan pacar saya. Saya merasa patah hati dan sedih sekali."
+
 url = 'http://localhost:5000/api'
-r = requests.post(url,json={'exp':1.8,})
+r = requests.get(f'{url}/search', params={'query': query_text})
+
+if r.status_code == 200:
+    # Parse the JSON response
+    song_titles = r.json()
+    print("Song Titles:")
+    for title in song_titles:
+        print(title)
+else:
+    print("Error:", r.text)
